19d82e21d06876f6dab2636a6ae3d9213ce467b9
lvlcn-t <75443136+lvlcn-t@users.noreply.github.com>
2024-05-04 19:20:55 +0200

    test: improve tests for effector run and go methods

--- executors/common_test.go
+++ executors/common_test.go
@@ -3,6 +3,7 @@ package executors
 import (
 	"context"
 	"errors"
+	"reflect"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -10,41 +11,175 @@ import (
 	"golang.org/x/time/rate"
 )
 
-func TestEffector_MultiplePolicies(t *testing.T) {
-	task := func(ctx context.Context) error {
-		// This task simulates a condition that can timeout or be rate-limited
+func TestEffector_Run(t *testing.T) {
+	applyAll := func(effector Effector, timeout time.Duration, rateLimit rate.Limit, circuitBreakerCount int, circuitBreakerTimeout time.Duration) Effector {
+		return effector.
+			WithRetry(Retrier{MaxRetries: 3, Backoff: func(retries uint) time.Duration { return 10 * time.Millisecond }}).
+			WithTimeout(timeout).
+			WithRateLimit(rateLimit).
+			WithCircuitBreaker(circuitBreakerCount, circuitBreakerTimeout).
+			WithProtection()
+	}
+
+	run := func(effector Effector, ctx context.Context) error { return effector.Do(ctx) }
+	runGo := func(effector Effector, ctx context.Context) error {
+		errChan := effector.Go(ctx)
 		select {
+		case err := <-errChan:
+			return err
 		case <-ctx.Done():
 			return ctx.Err()
-		case <-time.After(50 * time.Millisecond):
-			return errors.New("task failed")
 		}
 	}
 
-	retrier := Retrier{MaxRetries: 3, Backoff: func(retries int) time.Duration { return 10 * time.Millisecond }}
-	timeout := 100 * time.Millisecond
-	rateLimit := rate.Every(200 * time.Millisecond)
-	maxFailures := 1
-	resetTimeout := 500 * time.Millisecond
-
-	effector := Effector(task).
-		WithRetry(retrier).
-		WithTimeout(timeout).
-		WithRateLimit(rateLimit).
-		WithCircuitBreaker(maxFailures, resetTimeout).
-		WithProtection()
-
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
-
-	err := effector.Do(ctx)
-	if err == nil {
-		t.Error("Expected an error, but got none")
+	tests := []struct {
+		name     string
+		effector Effector
+		policies []func(Effector) Effector
+		run      func(Effector, context.Context) error
+		wantErr  bool
+		errType  reflect.Type
+	}{
+		{
+			name: "success",
+			effector: func(ctx context.Context) error {
+				return nil
+			},
+			run:     run,
+			wantErr: false,
+		},
+		{
+			name: "error",
+			effector: func(ctx context.Context) error {
+				return errors.New("task failed")
+			},
+			run:     run,
+			wantErr: true,
+			errType: reflect.TypeOf(errors.New("")),
+		},
+		{
+			name: "timeout",
+			effector: func(ctx context.Context) error {
+				select {
+				case <-ctx.Done():
+					return ctx.Err()
+				case <-time.After(50 * time.Millisecond):
+					return errors.New("task failed")
+				}
+			},
+			run: run,
+			policies: []func(Effector) Effector{
+				func(e Effector) Effector {
+					return e.WithTimeout(10 * time.Millisecond)
+				},
+			},
+			wantErr: true,
+			errType: reflect.TypeOf(context.DeadlineExceeded),
+		},
+		{
+			name: "rate limit",
+			effector: func(ctx context.Context) error {
+				return errors.New("task failed")
+			},
+			run: run,
+			policies: []func(Effector) Effector{
+				func(e Effector) Effector {
+					return e.WithRateLimit(rate.Every(200 * time.Millisecond))
+				},
+			},
+			wantErr: true,
+			errType: reflect.TypeOf(errors.New("")),
+		},
+		{
+			name: "circuit breaker",
+			effector: func(ctx context.Context) error {
+				return errors.New("task failed")
+			},
+			run: run,
+			policies: []func(Effector) Effector{
+				func(e Effector) Effector {
+					return e.WithCircuitBreaker(1, 500*time.Millisecond)
+				},
+			},
+			wantErr: true,
+			errType: reflect.TypeOf(ErrCircuitOpen{}),
+		},
+		{
+			name: "protection",
+			effector: func(ctx context.Context) error {
+				panic("task panicked")
+			},
+			run: run,
+			policies: []func(Effector) Effector{
+				func(e Effector) Effector {
+					return e.WithProtection()
+				},
+			},
+			wantErr: true,
+			errType: reflect.TypeOf(errors.Join(errors.New(""))),
+		},
+		{
+			name: "all policies with context timeout",
+			effector: func(ctx context.Context) error {
+				select {
+				case <-ctx.Done():
+					return ctx.Err()
+				case <-time.After(50 * time.Millisecond):
+					return errors.New("task failed")
+				}
+			},
+			run: run,
+			policies: []func(Effector) Effector{
+				func(e Effector) Effector {
+					return applyAll(e, 10*time.Millisecond, rate.Every(200*time.Millisecond), 2, 500*time.Millisecond)
+				},
+			},
+			wantErr: true,
+			errType: reflect.TypeOf(context.DeadlineExceeded),
+		},
+		{
+			name:     "all policies with success",
+			effector: noopEffector,
+			run:      run,
+			policies: []func(Effector) Effector{
+				func(e Effector) Effector {
+					return applyAll(e, 100*time.Millisecond, rate.Every(200*time.Millisecond), 1, 500*time.Millisecond)
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name:     "all policies with success parallel",
+			effector: noopEffector,
+			run:      runGo,
+			policies: []func(Effector) Effector{
+				func(e Effector) Effector {
+					return applyAll(e, 100*time.Millisecond, rate.Every(200*time.Millisecond), 1, 500*time.Millisecond)
+				},
+			},
+			wantErr: false,
+		},
 	}
 
-	// We cannot predict the exact error, but it should be either a deadline exceeded error or a circuit open error
-	if !errors.Is(err, context.DeadlineExceeded) && !errors.Is(err, ErrCircuitOpen) {
-		t.Errorf("Expected either %q or a %q error, but got %v", context.DeadlineExceeded.Error(), ErrCircuitOpen.Error(), err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			effector := tt.effector
+			for _, policy := range tt.policies {
+				effector = policy(effector)
+			}
+
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+			defer cancel()
+
+			err := tt.run(effector, ctx)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Effector.Do() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if tt.errType != nil && reflect.TypeOf(err) != tt.errType {
+				t.Errorf("Effector.Do() error = %v (%T), wantErr %v", err, err, tt.errType)
+			}
+		})
 	}
 }
 
