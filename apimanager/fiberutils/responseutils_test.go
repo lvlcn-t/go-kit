7ecc29dc810e5a85845b9875220281b5c3fd03f7
lvlcn-t <75443136+lvlcn-t@users.noreply.github.com>
2024-06-27 17:23:32 +0200

    test: add render view tests

--- apimanager/fiberutils/responseutils_test.go
+++ apimanager/fiberutils/responseutils_test.go
@@ -2,14 +2,17 @@ package fiberutils
 
 import (
 	"context"
+	"html/template"
 	"net/http"
+	"strings"
 	"testing"
 
+	"github.com/a-h/templ"
 	"github.com/gofiber/fiber/v3"
 	"github.com/google/go-cmp/cmp"
 )
 
-func Test_NewErrorResponse(t *testing.T) {
+func TestNewErrorResponse(t *testing.T) {
 	tests := []struct {
 		name   string
 		msg    string
@@ -101,6 +104,64 @@ func Test_Responses(t *testing.T) {
 
 			if resp.StatusCode != tt.wantStatus {
 				t.Errorf("got status %d, want %d", resp.StatusCode, tt.wantStatus)
+			}
+		})
+	}
+}
+
+func TestRenderView(t *testing.T) {
+	tests := []struct {
+		name     string
+		template *template.Template
+		options  []func(*templ.ComponentHandler)
+		wantCT   string
+	}{
+		{
+			name:     "without options",
+			template: template.Must(template.New("test").Parse("<h1>Hello, World!</h1>")),
+			options:  nil,
+			wantCT:   "text/html",
+		},
+		{
+			name:     "with options",
+			template: template.Must(template.New("test").Parse("<h1>Hello, World!</h1>")),
+			options: []func(*templ.ComponentHandler){
+				func(ch *templ.ComponentHandler) {
+					ch.ContentType = "text/plain"
+				},
+			},
+			wantCT: "text/plain",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			app := fiber.New()
+			app.Get("/", func(c fiber.Ctx) error {
+				return RenderView(c, templ.FromGoHTML(tt.template, nil), tt.options...)
+			})
+
+			req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, "/", http.NoBody)
+			if err != nil {
+				t.Fatalf("failed to create request: %v", err)
+			}
+
+			resp, err := app.Test(req)
+			if err != nil {
+				t.Fatalf("failed to send request: %v", err)
+			}
+			defer func() {
+				if err := resp.Body.Close(); err != nil {
+					t.Fatalf("failed to close response body: %v", err)
+				}
+			}()
+
+			if resp.StatusCode != http.StatusOK {
+				t.Errorf("got status %d, want %d", resp.StatusCode, http.StatusOK)
+			}
+
+			if !strings.Contains(resp.Header.Get("Content-Type"), tt.wantCT) {
+				t.Errorf("got content type %q, want %q", resp.Header.Get("Content-Type"), "text/html")
 			}
 		})
 	}
